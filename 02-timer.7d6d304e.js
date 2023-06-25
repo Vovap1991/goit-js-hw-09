6d777c50c002dc725843c0534c5969bef392f521
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-25 17:37:33 +0000

    Deploying to gh-pages from @ Vovap1991/goit-js-hw-09@766aadf03e4a68646c3e5b9f345fbca4c5ec73f7 🚀

---
+++ 02-timer.7d6d304e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".timer");e.style.fontSize="20px",e.style.gap="20px",e.style.display="flex";document.querySelectorAll(".value").forEach((e=>(e.style.fontSize="40px",e.style.display="flex",e.style.flexDirection="column",e.style.alignItems="center")));
+//# sourceMappingURL=02-timer.7d6d304e.js.map
