ea8f4c29eec97c72c55165a158cde2d6978132e5
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-25 20:54:42 +0300

    updated 2 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,35 @@
-const timerEl = document.querySelector(".timer");
-timerEl.style.fontSize = "20px";
-timerEl.style.gap = "20px";
-timerEl.style.display = "flex";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const startBtnEl = document.querySelector('button[data-start]');
+startBtnEl.disabled = true;
+const timerEl = document.querySelector('.timer');
+timerEl.style.fontSize = '20px';
+timerEl.style.gap = '20px';
+timerEl.style.display = 'flex';
 
 const fieldValueEls = document.querySelectorAll('.value');
 fieldValueEls.forEach(
-    element => (
-      (element.style.fontSize = "40px"),
-      (element.style.display = "flex"),
-      (element.style.flexDirection = "column"),
-      (element.style.alignItems = "center")
-    )
-  );
+  element => (
+    (element.style.fontSize = '40px'),
+    (element.style.display = 'flex'),
+    (element.style.flexDirection = 'column'),
+    (element.style.alignItems = 'center')
+  )
+);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    selectedDates = selectedDates[0];
+    if (selectedDates < new Date()) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+    }
+    startBtnEl.disabled = false;
+  },
+};
