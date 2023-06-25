1fdeea0e0271fe8a0909b17731aea50cc2269866
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-25 20:03:03 +0300

    finished task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const startColorSetting = () => {
+    startBtnEl.disabled = true;
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+};
+
+const stopColorSetting = () => {
+    startBtnEl.disabled = false;
+    clearInterval(timerId);
+};
+
+startBtnEl.addEventListener('click', startColorSetting);
+stopBtnEl.addEventListener('click', stopColorSetting);
