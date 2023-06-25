3bc58302130db8c7b18e6eb7e8a0acaabef41b6b
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-25 17:55:11 +0000

    Deploying to gh-pages from @ Vovap1991/goit-js-hw-09@ea8f4c29eec97c72c55165a158cde2d6978132e5 🚀

--- 02-timer.20cb59c7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".timer");e.style.fontSize="20px",e.style.gap="20px",e.style.display="flex",document.querySelectorAll(".value").forEach((function(e){return e.style.fontSize="40px",e.style.display="flex",e.style.flexDirection="column",e.style.alignItems="center"}))}();
-//# sourceMappingURL=02-timer.20cb59c7.js.map
