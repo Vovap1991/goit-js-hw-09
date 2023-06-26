291868417ec79702fe36e7fb3a415f65408cffaa
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-26 20:47:11 +0300

    Finished task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,6 +4,7 @@ import Notiflix from 'notiflix';
 
 const startBtnEl = document.querySelector('button[data-start]');
 startBtnEl.disabled = true;
+
 const timerEl = document.querySelector('.timer');
 timerEl.style.fontSize = '20px';
 timerEl.style.gap = '20px';
@@ -19,6 +20,12 @@ fieldValueEls.forEach(
   )
 );
 
+let selectedDate;
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -26,10 +33,45 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
-    selectedDates = selectedDates[0];
-    if (selectedDates < new Date()) {
+    selectedDate = selectedDates[0];
+    if (selectedDate < new Date()) {
       Notiflix.Notify.warning('Please choose a date in the future');
     }
     startBtnEl.disabled = false;
   },
 };
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function timerCounting() {
+  countingIntervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const remainingTime = selectedDate - currentTime;
+    startBtnEl.disabled = true;
+
+    const { days, hours, minutes, seconds } = convertMs(remainingTime);
+
+    daysEl.textContent = addLeadingZero(days);
+    hoursEl.textContent = addLeadingZero(hours);
+    minutesEl.textContent = addLeadingZero(minutes);
+    secondsEl.textContent = addLeadingZero(seconds);
+  }, 1000);
+}
+
+startBtnEl.addEventListener('click', timerCounting);
+flatpickr('#datetime-picker', options);
