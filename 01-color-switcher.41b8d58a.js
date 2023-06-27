9c1bea097e70805108b734dbd140cdde3ca25289
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-27 19:32:01 +0000

    Deploying to gh-pages from @ Vovap1991/goit-js-hw-09@c1ebf318326b8881a8a84e506f0f6a71c85239f9 🚀

---
+++ 01-color-switcher.41b8d58a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0;setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.41b8d58a.js.map
